fd673a2edfa6c35cb9373254f094e1ac68c7ce20
watermelonpro <dan.noodle@hotmail.com>
2024-02-09 17:24:02 -0800

    Co-authored-by: Oscar Yamada <OscarYamada@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+</head>
+<body> <!--The main view of the food in the first page-->
+    <div class="box1">
+    <h3>Burger 1</h3>
+    <p>Chicken Burger</p>
+    </div>
+</body>
+<style>
+    .box1 {
+        background-color: rgba(0,0,0,.5);
+        color: #fff;
+    }
+</style>
+
